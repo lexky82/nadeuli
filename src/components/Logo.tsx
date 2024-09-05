88cd30866fc575868a61bf4e1a79fa9e5e2637ac
lexky82 <lexky@naver.com>
2024-09-05 11:29:05 +0900

    feat: Logo 클릭시 "/"으로 이동 LInk 컴포넌트 래핑

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Image from "next/image";
+import Link from "next/link";
 
 const Title = styled.span`
   font-weight: bold;
@@ -17,15 +18,17 @@ const LogoWrapper = styled.div`
 
 export default function Logo() {
   return (
-    <LogoWrapper>
-      <Image
-        src="/logo.svg" // public 디렉토리 내의 파일은 '/' 경로로 시작합니다.
-        alt="Logo"
-        width={32} // 로고의 가로 크기
-        height={32} // 로고의 세로 크기
-      />
+    <Link href="/">
+      <LogoWrapper>
+        <Image
+          src="/logo.svg" // public 디렉토리 내의 파일은 '/' 경로로 시작합니다.
+          alt="Logo"
+          width={32} // 로고의 가로 크기
+          height={32} // 로고의 세로 크기
+        />
 
-      <Title>나들이</Title>
-    </LogoWrapper>
+        <Title>나들이</Title>
+      </LogoWrapper>
+    </Link>
   );
 }
