767ec9129d001824af7e67a6b96fbc7267a85e81
lexky82 <lexky@naver.com>
2024-09-06 22:34:31 +0900

    feat: LoginForm 초기 퍼블리싱

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -1,17 +1,67 @@
 "use client";
-import GlassBox from "@/components/atiom/GlassBox";
-import Logo from "@/components/Logo";
-import Heading from "@/components/atiom/Heading";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  LoginFormWrapper,
+  FormFieldWrapper,
+  FormInput,
+  LoginOptionsWrapper,
+  FindLinkBox,
+  Submit,
+  RecomendSignup,
+  OrSeparator,
+  SocialLogin,
+  GoogleLoginBox,
+} from "./style";
 
 const LoginForm = () => {
   return (
-    <GlassBox width={800} height={600} padding="40px">
-      <Logo />
+    <LoginFormWrapper>
+      <FormFieldWrapper>
+        <label>이메일</label>
+        <FormInput id="email" type="text" width={380} placeholder="이메일을 입력해주세요." />
+      </FormFieldWrapper>
 
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Heading>안녕하세요 나들이입니다.</Heading>
-      </div>
-    </GlassBox>
+      <FormFieldWrapper>
+        <label>패스워드</label>
+        <FormInput
+          id="password"
+          type="password"
+          width={380}
+          placeholder="패스워드를 입력해주세요."
+        />
+      </FormFieldWrapper>
+
+      <LoginOptionsWrapper>
+        <div>
+          <input type="checkbox" name="autoLogin" id="autoLogin" />
+          <span>자동 로그인</span>
+        </div>
+
+        <FindLinkBox>
+          <Link href="/findid">이메일 찾기</Link>
+          <Link href="/findpw">패스워드 찾기</Link>
+        </FindLinkBox>
+      </LoginOptionsWrapper>
+
+      <Submit type="submit">로그인</Submit>
+
+      <RecomendSignup>
+        <span>계정이 없다면?</span>
+        <Link href="/signup">회원가입</Link>
+      </RecomendSignup>
+
+      <OrSeparator>
+        <span>or</span>
+      </OrSeparator>
+
+      <SocialLogin>
+        <GoogleLoginBox>
+          <Image src="/web_neutral_rd_na 1.svg" alt="google" width={40} height={40} />
+          <span>구글 계정으로 로그인</span>
+        </GoogleLoginBox>
+      </SocialLogin>
+    </LoginFormWrapper>
   );
 };
 
