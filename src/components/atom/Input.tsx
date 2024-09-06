a7f082a3a77ee3842e967914a4cc66e420594860
lexky82 <lexky@naver.com>
2024-09-06 22:32:38 +0900

    feat: Input type="text" 컴포넌트 추가

---
+++ src/components/atom/Input.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import styled from "styled-components";
+
+const Input = styled.input<{ width?: number }>`
+  width: ${(props) => props.width}px;
+  height: 24px;
+  padding: 10px 15px;
+  background-color: #d9d9d9;
+  border-radius: 15px;
+  filter: brightness(1);
+  outline: none;
+  border: 1px solid ${(props) => props.theme.colors.gray};
+
+  &:focus {
+    background-color: #ffffff;
+    outline: 1px solid ${(props) => props.theme.colors.primary};
+  }
+`;
+
+export default Input;
