d6c9467f2581f32652a53317ae590fb2c5bede42
lexky82 <lexky@naver.com>
2024-09-07 17:24:10 +0900

    refactor: width 값을 props로 받아서 처리하는 것 삭제

--- src/components/atom/Input.tsx
+++ src/components/atom/Input.tsx
@@ -2,8 +2,7 @@
 
 import styled from "styled-components";
 
-const Input = styled.input<{ width?: number }>`
-  width: ${(props) => props.width}px;
+const Input = styled.input`
   height: 24px;
   padding: 10px 15px;
   background-color: #d9d9d9;
