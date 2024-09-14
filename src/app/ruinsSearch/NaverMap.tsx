edb01ef4e9f84a9429919a6c3c2d0c9c94fcb029
lexky82 <lexky@naver.com>
2024-09-14 23:07:30 +0900

    refactor: useMap훅 mapId만 필수로 받게하고 관련 option값들은 object로 받고 optional하게 받도록 수정

--- src/app/ruinsSearch/NaverMap.tsx
+++ src/app/ruinsSearch/NaverMap.tsx
@@ -4,12 +4,12 @@ import useMap from "@/hook/useMap";
 import { useEffect, useRef } from "react";
 
 export default function NaverMap() {
-  const { initializeMap } = useMap("map", [37.3595704, 127.105399], { width: 1920, height: 918 });
   const mapRef = useRef<HTMLElement | null>(null);
+  const { initializeMap } = useMap("map", { size: { width: 1920, height: 918 } });
 
   useEffect(() => {
     initializeMap();
   }, [mapRef]);
 
-  return <div id="map"></div>;
+  return <div id="map" />;
 }
--- src/hook/useMap.ts
+++ src/hook/useMap.ts
@@ -1,17 +1,23 @@
 import { useCallback } from "react";
 
 type mapId = string;
-type center = [number, number];
-type size = { width: number; height: number };
 
-const useMap = (mapId: mapId, center: center, size: size) => {
+interface mapOptionType {
+  center?: [number, number];
+  size?: { width: number; height: number };
+  zoom?: number;
+}
+
+const useMap = (mapId: mapId, { center, size, zoom }: mapOptionType) => {
   const initializeMap = useCallback(() => {
     let nmap: naver.maps.Map;
 
     nmap = new naver.maps.Map(mapId, {
-      center: new naver.maps.LatLng(...center),
-      size: size,
-      zoom: 16,
+      center: center
+        ? new naver.maps.LatLng(...center)
+        : new naver.maps.LatLng(37.3595704, 127.105399),
+      size: size ? size : { width: 400, height: 400 },
+      zoom: zoom ? zoom : 16,
     });
   }, []);
 
@@ -19,4 +25,5 @@ const useMap = (mapId: mapId, center: center, size: size) => {
     initializeMap,
   };
 };
+
 export default useMap;
