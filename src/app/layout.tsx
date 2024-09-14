aafec4f8f9d5ef82d124d8fc94fc7c78b14ae988
lexky82 <lexky@naver.com>
2024-09-14 15:09:25 +0900

    feat: Naver Map API 호출

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,26 @@ import "swiper/css";
 
 import Nav from "@/components/Nav";
 import { themeClass } from "@/styles/globalTheme.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko" className={themeClass}>
+    <html lang="ko-kr" className={themeClass}>
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
           rel="stylesheet"
+        />
+        <Script
+          strategy="beforeInteractive"
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_ID}`}
         />
       </head>
       <body>
         <header>
           <Nav />
         </header>
+
         <main>{children}</main>
       </body>
     </html>
