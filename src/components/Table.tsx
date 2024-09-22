2980e05671fea34a06f11bc277929d9734ccab55
lexky82 <lexky@naver.com>
2024-09-22 17:13:04 +0900

    feat: Table 컴포넌트 구현

---
+++ src/components/Table.tsx
@@ -0,0 +1,45 @@
+import * as styles from "@/styles/components/Table.css";
+
+interface tableColumn {
+  title: string;
+  dataIndex: string;
+  render?: (data: string) => React.ReactNode;
+}
+
+interface data {
+  key: React.Key;
+  [key: string]: any;
+}
+
+interface TableProps {
+  columns: tableColumn[];
+  dataSource: data[];
+}
+
+export const Table = ({ columns, dataSource }: TableProps) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th className={styles.tableColumn} key={column.dataIndex}>
+              {column.title}
+            </th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {dataSource.map((data) => (
+          <tr key={data.key}>
+            {columns.map((column) => (
+              <td key={column.dataIndex} className={styles.tableRow}>
+                {column.render ? column.render(data[column.dataIndex]) : data[column.dataIndex]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
