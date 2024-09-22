896b26823da06575816a8459aca04793db945532
lexky82 <lexky@naver.com>
2024-09-22 17:15:01 +0900

    style: Table 스타일 분리

---
+++ src/styles/components/Table.css.ts
@@ -0,0 +1,11 @@
+import { style, createVar } from "@vanilla-extract/css";
+
+export const tableColumn = style({
+  fontSize: 24,
+  padding: 10,
+});
+
+export const tableRow = style({
+  textAlign: "center",
+  padding: 5,
+});
