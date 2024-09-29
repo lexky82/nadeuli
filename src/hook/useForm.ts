47ef782ebd18c8aba73f749f01a12fcf0cd29597
lexky82 <lexky@naver.com>
2024-09-29 13:52:10 +0900

    feat: 폼을 관리하는 useForm 커스텀 훅 구현

---
+++ src/hook/useForm.ts
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { ZodSchema, z } from "zod";
+
+export const useForm = <T extends ZodSchema<any>>(initialValues: z.infer<T>, validateSchema: T) => {
+  const [formData, setFormData] = useState(initialValues);
+  const [errors, setErrors] = useState<{ [key: string]: string | undefined }>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+
+    const validationResult = validateSchema.safeParse({ ...formData, [id]: value });
+
+    if (!validationResult.success) {
+      const zodError = validationResult.error.issues
+        .map((zIssue) => {
+          return { [zIssue.path[0]]: zIssue.message };
+        })
+        .reduce((acc, curr) => ({ ...acc, ...curr }), {});
+
+      setErrors(zodError);
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, [id]: undefined }));
+    }
+  };
+
+  return { formData, errors, handleChange };
+};
