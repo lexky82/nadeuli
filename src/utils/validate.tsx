c55fe1a3f2347ffa5292df809c67b8e7bfbe174c
lexky82 <lexky@naver.com>
2024-10-02 20:00:31 +0900

    feat: 패스워드 확인 유효식 추가

--- src/utils/validate.tsx
+++ src/utils/validate.tsx
@@ -1,14 +1,19 @@
 import { z } from "zod";
 
-export const signupSchema = z.object({
-  email: z.string().email("이메일 형식이 아닙니다."),
-  phone: z.string(),
-  nickName: z.string(),
-  password: z
-    .string()
-    .min(8, "8자 이상 16자 이하")
-    .max(16, "8자 이상 16자 이하")
-    .regex(/\d/, "숫자 조합 사용")
-    .regex(/[!@#$%^&*(),.?":{}|<>]/, "특수 문자 포함 사용"),
-  passwordConfirm: z.string(),
-});
+export const signupSchema = z
+  .object({
+    email: z.string().email("이메일 형식이 아닙니다."),
+    phone: z.string(),
+    nickName: z.string(),
+    password: z
+      .string()
+      .min(8, "8자 이상 16자 이하")
+      .max(16, "8자 이상 16자 이하")
+      .regex(/\d/, "숫자 조합 사용")
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, "특수 문자 포함 사용"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "패스워드와 일치하지 않습니다.",
+    path: ["confirmPassword"],
+  });
