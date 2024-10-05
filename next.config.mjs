a554c33c1f62dcd7f45c242d9d48d2ac9fa02104
lexky82 <lexky@naver.com>
2024-10-05 16:58:15 +0900

    feat: proxy 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
