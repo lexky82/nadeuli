74101f19e31869597e9d2919def46b915098a2f5
lexky82 <lexky@naver.com>
2024-10-07 14:13:02 +0900

    feat: 사용하지 않는 phone 삭제

--- src/utils/validate.tsx
+++ src/utils/validate.tsx
@@ -3,7 +3,6 @@ import { z } from "zod";
 export const signupSchema = z
   .object({
     email: z.string().email("이메일 형식이 아닙니다."),
-    phone: z.string(),
     nickName: z.string(),
     password: z
       .string()
