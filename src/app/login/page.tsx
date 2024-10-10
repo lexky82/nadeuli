33701b6ab58b697d64e58b0644724fe28bb8b55e
lexky82 <lexky@naver.com>
2024-10-10 12:47:39 +0900

    feat: 세션이 있을 시 로그인 창 진입 못하도록 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,16 @@ import Logo from "@/components/atom/Logo";
 import LoginForm from "./LoginForm";
 import Image from "next/image";
 import { GlassBox } from "@/components/atom/GlassBox";
+import { auth } from "../auth";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/");
+  }
+
   return (
     <div className={style.fullScreenContainer}>
       <Image
