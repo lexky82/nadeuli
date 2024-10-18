5cedfc12d7daf34ad951518bedc7e56fb17b54ef
lexky82 <lexky@naver.com>
2024-10-18 20:46:08 +0900

    feat: Tabs 컴포넌트 디자인 변경 및 현재 렌더링 중인 Tab을 알려주는 bar 요소 추가

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as styles from "@/styles/components/Tabs.css";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
 
 interface TabItem {
   key: number;
@@ -17,6 +18,9 @@ interface TabsProps {
 
 const Tabs = ({ defaultTabIndex = 0, items, onChange }: TabsProps) => {
   const [activeKey, setActiveKey] = useState(defaultTabIndex);
+  const [barWidth, setBarWidth] = useState(0);
+  const [barLeft, setBarLeft] = useState(0);
+  const tabRefs = useRef<HTMLDivElement[] | null[]>([]);
 
   const handleTabClick = (index: number) => {
     setActiveKey(index);
@@ -26,20 +30,42 @@ const Tabs = ({ defaultTabIndex = 0, items, onChange }: TabsProps) => {
     }
   };
 
+  useEffect(() => {
+    if (tabRefs.current) {
+      const currentTab = tabRefs.current[activeKey];
+
+      if (currentTab) {
+        setBarWidth(currentTab.clientWidth);
+        setBarLeft(currentTab.offsetLeft);
+      }
+    }
+  }, [activeKey]);
+
   return (
     <div>
       <div className={styles.tabLabelWrapper}>
-        {items.map((item) => {
+        {items.map((item, index) => {
           return (
             <div
               key={item.key}
               onClick={() => handleTabClick(item.key)}
               className={styles.tabLabel}
+              style={assignInlineVars({
+                [styles.activeColor]: activeKey === index ? "#0033A0" : "#c1c1c1",
+              })}
+              ref={(el) => {
+                tabRefs.current[index] = el;
+              }}
             >
               {item.label}
             </div>
           );
         })}
+
+        <span
+          className={styles.tabsIndicator}
+          style={{ width: `${barWidth}px`, left: `${barLeft}px` }}
+        />
       </div>
 
       {items.filter((item) => item.key === activeKey)[0]?.children}
--- src/styles/components/Tabs.css.ts
+++ src/styles/components/Tabs.css.ts
@@ -2,14 +2,29 @@ import { themeVars } from "@/styles/globalTheme.css";
 import { style, createVar } from "@vanilla-extract/css";
 
 export const tabLabelWrapper = style({
+  position: "relative",
   display: "flex",
+  justifyContent: "center",
   fontSize: 18,
 });
 
-export const activeBackground = createVar();
-export const activeBoxShadow = createVar();
+export const activeColor = createVar();
 
 export const tabLabel = style({
   cursor: "pointer",
   userSelect: "none",
+  color: activeColor,
+
+  ":hover": {
+    color: themeVars.colors.primary,
+  },
+});
+
+export const tabsIndicator = style({
+  position: "absolute",
+  bottom: 0,
+  width: 80,
+  height: 2,
+  background: themeVars.colors.primary,
+  transition: "all 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
 });
