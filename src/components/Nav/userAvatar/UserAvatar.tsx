086269ce99d79c84200896beb3d4ddb8e46372d0
lexky82 <lexky@naver.com>
2024-10-18 20:47:16 +0900

    feat: nav 클릭 시 해당 Path로 이동 구현

--- src/components/Nav/userAvatar/UserAvatar.tsx
+++ src/components/Nav/userAvatar/UserAvatar.tsx
@@ -5,9 +5,12 @@ import Image from "next/image";
 import * as styles from "@/styles/components/Nav.css";
 import * as avatarStyles from "./userAvatar.css";
 import { signOutWithForm } from "@/app/serverActions/auth";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const UserAvatar = () => {
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const router = useRouter();
 
   const handleAvatarClick = () => {
     setIsVisible(!isVisible);
@@ -21,6 +24,11 @@ export const UserAvatar = () => {
     signOutWithForm().then(() => {
       window.location.reload();
     });
+  };
+
+  const handleRouteClick = (route: string) => {
+    router.push(route);
+    setIsVisible(false);
   };
 
   return (
@@ -46,13 +54,18 @@ export const UserAvatar = () => {
           </div>
 
           <div className={avatarStyles.profileNavWrapper}>
-            <div className={avatarStyles.profileNav}>
-              <Image src="Account_manage.svg" alt="manage_profile" width={24} height={24} />
+            <div
+              className={avatarStyles.profileNav}
+              onClick={() => handleRouteClick("/account/accountSetting")}
+            >
+              <Image src="/Account_manage.svg" alt="manage_profile" width={24} height={24} />
               <span>계정관리</span>
             </div>
-
-            <div className={avatarStyles.profileNav}>
-              <Image src="history.svg" alt="my_history" width={24} height={24} />
+            <div
+              className={avatarStyles.profileNav}
+              onClick={() => handleRouteClick("/account/history")}
+            >
+              <Image src="/history.svg" alt="my_history" width={24} height={24} />
               <span>나의 활동</span>
             </div>
 
@@ -60,7 +73,7 @@ export const UserAvatar = () => {
               className={`${avatarStyles.profileNav} ${avatarStyles.logout}`}
               onClick={handleLogoutClick}
             >
-              <Image src="logout.svg" alt="logout" width={24} height={24} />
+              <Image src="/logout.svg" alt="logout" width={24} height={24} />
               <span>로그아웃</span>
             </div>
           </div>
