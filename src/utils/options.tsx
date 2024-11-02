3e9a246393889d6b6978677f06cf638933ed84e6
lexky82 <lexky@naver.com>
2024-11-02 15:47:15 +0900

    feat: 주소, 카테고리 옵션 파일 따로 관리

---
+++ src/utils/options.tsx
@@ -0,0 +1,24 @@
+export const LOCATION_OPTION = [
+  { id: 1, name: "서울" },
+  { id: 2, name: "경기북부" },
+  { id: 3, name: "경기남부" },
+  { id: 4, name: "강원도" },
+  { id: 5, name: "충청북도" },
+  { id: 6, name: "충청남도" },
+  { id: 7, name: "전라북도" },
+  { id: 8, name: "전라남도" },
+  { id: 9, name: "경상북도" },
+  { id: 10, name: "경상남도" },
+  { id: 11, name: "제주도" },
+];
+
+export const CATEGORY_OPTION = [
+  { id: 1, name: "고분" },
+  { id: 2, name: "고택" },
+  { id: 3, name: "고궁" },
+  { id: 4, name: "방어시설" },
+  { id: 5, name: "유적사적" },
+  { id: 6, name: "터" },
+  { id: 7, name: "서원" },
+  { id: 8, name: "사찰" },
+];
