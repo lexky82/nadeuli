238af3791afb156b80d6a4f33bf002e963196383
lexky82 <lexky@naver.com>
2024-11-04 08:39:52 +0900

    style: 유적지 추천 카드 스타일 개선

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
     <div>
       <HomeShowcase />
 
-      <div style={{ padding: "0 50px" }}>
+      <div style={{ padding: "10px 50px" }}>
         <h3 style={{ color: themeVars.colors.primary }}>추천 유적 관광지</h3>
 
-        <div style={{ display: "flex", gap: 20 }}>
+        <div style={{ display: "flex", gap: 20, fontSize: 14 }}>
           <div
             style={{
-              width: 300,
-              height: 400,
+              width: 400,
+              height: 550,
               borderRadius: 15,
               position: "relative",
               boxShadow: "0 4px 4px rgba(0,0,0,0.3)",
@@ -39,7 +39,8 @@ export default function Home() {
                 background: "rgba(0,0,0,0.7)",
                 width: "calc(100% - 60px)",
                 color: "white",
-                padding: "10px 30px",
+                padding: "5px 30px",
+                gap: 10,
               }}
             >
               <div
@@ -49,282 +50,23 @@ export default function Home() {
                   justifyContent: "space-between",
                 }}
               >
-                <span>수원화성</span>
+                <span style={{ fontWeight: "bold", fontSize: 16 }}>삼년산성</span>
 
                 <div style={{ display: "flex", alignItems: "center", gap: 5 }}>
-                  <Image src="/Thumb_up.svg" width={24} height={24} alt="좋아요" />
+                  <Image src="/Thumb_up.svg" width={18} height={18} alt="좋아요" />
                   <span>423</span>
                 </div>
               </div>
 
-              <div style={{ display: "flex", alignItems: "center", marginTop: 15, fontSize: 15 }}>
-                <Image src="/Address.svg" width={18} height={18} alt="위치" />
-                <span>경기도, 수원</span>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{
-              width: 300,
-              height: 400,
-              borderRadius: 15,
-              position: "relative",
-              boxShadow: "0 4px 4px rgba(0,0,0,0.3)",
-            }}
-          >
-            <Image
-              fill
-              src="/ruinsTopExampe.jpg"
-              alt=""
-              style={{ objectFit: "fill", borderRadius: 15 }}
-            />
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                position: "absolute",
-                bottom: 0,
-                height: 60,
-                borderRadius: 15,
-                background: "rgba(0,0,0,0.7)",
-                width: "calc(100% - 60px)",
-                color: "white",
-                padding: "10px 30px",
-              }}
-            >
               <div
                 style={{
                   display: "flex",
                   alignItems: "center",
-                  justifyContent: "space-between",
+                  gap: 5,
                 }}
               >
-                <span>수원화성</span>
-
-                <div style={{ display: "flex", alignItems: "center", gap: 5 }}>
-                  <Image src="/Thumb_up.svg" width={24} height={24} alt="좋아요" />
-                  <span>423</span>
-                </div>
-              </div>
-
-              <div style={{ display: "flex", alignItems: "center", marginTop: 15, fontSize: 15 }}>
                 <Image src="/Address.svg" width={18} height={18} alt="위치" />
-                <span>경기도, 수원</span>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{
-              width: 300,
-              height: 400,
-              borderRadius: 15,
-              position: "relative",
-              boxShadow: "0 4px 4px rgba(0,0,0,0.3)",
-            }}
-          >
-            <Image
-              fill
-              src="/ruinsTopExampe.jpg"
-              alt=""
-              style={{ objectFit: "fill", borderRadius: 15 }}
-            />
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                position: "absolute",
-                bottom: 0,
-                height: 60,
-                borderRadius: 15,
-                background: "rgba(0,0,0,0.7)",
-                width: "calc(100% - 60px)",
-                color: "white",
-                padding: "10px 30px",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <span>수원화성</span>
-
-                <div style={{ display: "flex", alignItems: "center", gap: 5 }}>
-                  <Image src="/Thumb_up.svg" width={24} height={24} alt="좋아요" />
-                  <span>423</span>
-                </div>
-              </div>
-
-              <div style={{ display: "flex", alignItems: "center", marginTop: 15, fontSize: 15 }}>
-                <Image src="/Address.svg" width={18} height={18} alt="위치" />
-                <span>경기도, 수원</span>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{
-              width: 300,
-              height: 400,
-              borderRadius: 15,
-              position: "relative",
-              boxShadow: "0 4px 4px rgba(0,0,0,0.3)",
-            }}
-          >
-            <Image
-              fill
-              src="/ruinsTopExampe.jpg"
-              alt=""
-              style={{ objectFit: "fill", borderRadius: 15 }}
-            />
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                position: "absolute",
-                bottom: 0,
-                height: 60,
-                borderRadius: 15,
-                background: "rgba(0,0,0,0.7)",
-                width: "calc(100% - 60px)",
-                color: "white",
-                padding: "10px 30px",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <span>수원화성</span>
-
-                <div style={{ display: "flex", alignItems: "center", gap: 5 }}>
-                  <Image src="/Thumb_up.svg" width={24} height={24} alt="좋아요" />
-                  <span>423</span>
-                </div>
-              </div>
-
-              <div style={{ display: "flex", alignItems: "center", marginTop: 15, fontSize: 15 }}>
-                <Image src="/Address.svg" width={18} height={18} alt="위치" />
-                <span>경기도, 수원</span>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{
-              width: 300,
-              height: 400,
-              borderRadius: 15,
-              position: "relative",
-              boxShadow: "0 4px 4px rgba(0,0,0,0.3)",
-            }}
-          >
-            <Image
-              fill
-              src="/ruinsTopExampe.jpg"
-              alt=""
-              style={{ objectFit: "fill", borderRadius: 15 }}
-            />
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                position: "absolute",
-                bottom: 0,
-                height: 60,
-                borderRadius: 15,
-                background: "rgba(0,0,0,0.7)",
-                width: "calc(100% - 60px)",
-                color: "white",
-                padding: "10px 30px",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <span>수원화성</span>
-
-                <div style={{ display: "flex", alignItems: "center", gap: 5 }}>
-                  <Image src="/Thumb_up.svg" width={24} height={24} alt="좋아요" />
-                  <span>423</span>
-                </div>
-              </div>
-
-              <div style={{ display: "flex", alignItems: "center", marginTop: 15, fontSize: 15 }}>
-                <Image src="/Address.svg" width={18} height={18} alt="위치" />
-                <span>경기도, 수원</span>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{
-              width: 300,
-              height: 400,
-              borderRadius: 15,
-              position: "relative",
-              boxShadow: "0 4px 4px rgba(0,0,0,0.3)",
-            }}
-          >
-            <Image
-              fill
-              src="/ruinsTopExampe.jpg"
-              alt=""
-              style={{ objectFit: "fill", borderRadius: 15 }}
-            />
-
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                position: "absolute",
-                bottom: 0,
-                height: 60,
-                borderRadius: 15,
-                background: "rgba(0,0,0,0.7)",
-                width: "calc(100% - 60px)",
-                color: "white",
-                padding: "10px 30px",
-              }}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <span>수원화성</span>
-
-                <div style={{ display: "flex", alignItems: "center", gap: 5 }}>
-                  <Image src="/Thumb_up.svg" width={24} height={24} alt="좋아요" />
-                  <span>423</span>
-                </div>
-              </div>
-
-              <div style={{ display: "flex", alignItems: "center", marginTop: 15, fontSize: 15 }}>
-                <Image src="/Address.svg" width={18} height={18} alt="위치" />
-                <span>경기도, 수원</span>
+                <span>충청북도, 보은</span>
               </div>
             </div>
           </div>
