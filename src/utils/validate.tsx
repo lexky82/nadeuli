f93bd86e34137674903a121daea824fb1aa0b6fc
lexky82 <lexky@naver.com>
2024-11-07 14:28:15 +0900

    feat: 비밀번호 변경 폼 스키마 추가

--- src/utils/validate.tsx
+++ src/utils/validate.tsx
@@ -20,3 +20,18 @@ export const signupSchema = z
 export const findPwSchema = z.object({
   email: z.string().email("이메일 형식이 아닙니다."),
 });
+
+export const changePwSchema = z
+  .object({
+    newPassword: z
+      .string()
+      .min(8, "8자 이상 16자 이하")
+      .max(16, "8자 이상 16자 이하")
+      .regex(/\d/, "숫자 조합 사용")
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, "특수 문자 포함 사용"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: "패스워드와 일치하지 않습니다.",
+    path: ["confirmPassword"],
+  });
