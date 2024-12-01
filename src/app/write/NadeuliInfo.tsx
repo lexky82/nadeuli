85acfbf0ce243df587297187398bcec170a11e1d
lexky82 <lexky@naver.com>
2024-12-01 19:41:17 +0900

    feat: 나들이 정보 입력 스텝 페이지 퍼블리싱

---
+++ src/app/write/NadeuliInfo.tsx
@@ -0,0 +1,220 @@
+import { Button } from "@/components/atom/Button";
+import { themeVars } from "@/styles/globalTheme.css";
+
+const NadeuliInfo = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 35,
+        padding: "50px 100px",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ display: "flex", gap: 20, alignItems: "center" }}>
+        <label style={{ width: 80 }} htmlFor="title">
+          기행문 제목
+        </label>
+
+        <input
+          type="text"
+          name="title"
+          id="title"
+          style={{
+            width: 400,
+            height: 24,
+            borderRadius: 5,
+            borderColor: themeVars.colors.gray,
+            outline: "none",
+          }}
+        />
+      </div>
+
+      <div style={{ display: "flex", gap: 20, alignItems: "center" }}>
+        <label style={{ width: 80 }} htmlFor="location">
+          유적지 위치
+        </label>
+
+        <input
+          type="text"
+          name="location"
+          id="location"
+          style={{
+            width: 400,
+            height: 24,
+            borderRadius: 5,
+            outline: "none",
+            borderColor: themeVars.colors.gray,
+          }}
+        />
+      </div>
+
+      <div style={{ display: "flex", gap: 20 }}>
+        <label style={{ width: 80 }} htmlFor="date">
+          방문 날짜
+        </label>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          max={new Date().toISOString().split("T")[0]}
+          style={{
+            width: 230,
+            borderRadius: 5,
+            borderColor: themeVars.colors.gray,
+            fontWeight: "bold",
+          }}
+        />
+      </div>
+
+      <div style={{ display: "flex", gap: 10, flexDirection: "column" }}>
+        <label style={{ width: 80 }}>날씨</label>
+
+        <div style={{ display: "flex", gap: 10 }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderColor: themeVars.colors.gray,
+              borderRadius: 15,
+              cursor: "pointer",
+            }}
+          >
+            맑음
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderRadius: 15,
+              cursor: "pointer",
+              borderColor: themeVars.colors.gray,
+            }}
+          >
+            흐림
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderRadius: 15,
+              cursor: "pointer",
+              borderColor: themeVars.colors.gray,
+            }}
+          >
+            비
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderRadius: 15,
+              cursor: "pointer",
+              borderColor: themeVars.colors.gray,
+            }}
+          >
+            눈
+          </div>
+        </div>
+      </div>
+
+      <div style={{ display: "flex", gap: 10, flexDirection: "column" }}>
+        <label style={{ width: 80 }} htmlFor="title">
+          방문 인원
+        </label>
+
+        <div style={{ display: "flex", gap: 10 }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderColor: themeVars.colors.gray,
+              borderRadius: 15,
+              cursor: "pointer",
+            }}
+          >
+            1~5
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderRadius: 15,
+              cursor: "pointer",
+              borderColor: themeVars.colors.gray,
+            }}
+          >
+            6~10
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderRadius: 15,
+              cursor: "pointer",
+              borderColor: themeVars.colors.gray,
+            }}
+          >
+            10~30
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 100,
+              height: 80,
+              fontWeight: "bold",
+              border: "1px solid",
+              borderRadius: 15,
+              cursor: "pointer",
+              borderColor: themeVars.colors.gray,
+            }}
+          >
+            30~
+          </div>
+        </div>
+
+        <Button style={{ borderRadius: 5, width: 187, height: 35, marginTop: 35 }}>계속</Button>
+      </div>
+    </div>
+  );
+};
+
+export default NadeuliInfo;
