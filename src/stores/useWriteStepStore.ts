114bc1e5a10a3081daae41f18af042db1aada82a
lexky82 <lexky@naver.com>
2024-12-04 14:26:58 +0900

    refactor: 기행문 작성 관련 전역상태 구조 개선

--- src/stores/useWriteStepStore.ts
+++ src/stores/useWriteStepStore.ts
@@ -2,6 +2,25 @@ import { create } from "zustand";
 
 interface WriteStore {
   currentStep: number;
+  steps: [
+    {
+      id: 1;
+      isCompleted: boolean;
+      isValid: boolean;
+      data: {
+        nadeuliInfo: {
+          title: string;
+          location: string;
+          date: string;
+          wheather: string;
+          visitor: number;
+        };
+      };
+    },
+    { id: 2; isCompleted: boolean; isValid: boolean; data: { travelogue: string } },
+    { id: 3; isCompleted: boolean; isValid: boolean; data: {} }
+  ];
+
   nextStep: () => void;
   prevStep: () => void;
   changeStep: (step: number) => void;
@@ -10,10 +29,35 @@ interface WriteStore {
 
 const useWriteStore = create<WriteStore>((set) => ({
   currentStep: 1,
+
+  steps: [
+    {
+      id: 1,
+      isCompleted: false,
+      isValid: false,
+      data: { nadeuliInfo: { title: "", location: "", date: "", wheather: "", visitor: 0 } },
+    },
+    { id: 2, isCompleted: false, isValid: false, data: { travelogue: "" } },
+    { id: 3, isCompleted: false, isValid: false, data: {} },
+  ],
+
   nextStep: () => set((state) => ({ currentStep: state.currentStep + 1 })),
   prevStep: () => set((state) => ({ currentStep: state.currentStep - 1 })),
   changeStep: (step) => set(() => ({ currentStep: step })),
-  clearStore: () => set(() => ({ currentStep: 1, verifyEmail: "" })),
+  clearStore: () =>
+    set(() => ({
+      currentStep: 1,
+      steps: [
+        {
+          id: 1,
+          isCompleted: false,
+          isValid: false,
+          data: { nadeuliInfo: { title: "", location: "", date: "", wheather: "", visitor: 0 } },
+        },
+        { id: 2, isCompleted: false, isValid: false, data: { travelogue: "" } },
+        { id: 3, isCompleted: false, isValid: false, data: {} },
+      ],
+    })),
 }));
 
 export default useWriteStore;
